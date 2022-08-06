66262152d10160b0ce62ed0eac3bbd2e8de6ef13
Shubh Jangam <shubh.jangam@chitran.in>
2022-08-06 22:21:14 +0530

    Base Table - Streams Done & Router Updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const authRoute = require("./routes/auth");
 const countryRoute = require("./routes/base_tables/country");
 const stateRoute = require("./routes/base_tables/state");
 const districtRoute = require("./routes/base_tables/district");
+const universityRoute = require("./routes/base_tables/university")
+const streamRoute = require("./routes/base_tables/stream");
+const roleRouter = require("./routes/role");
 require("dotenv").config();
 
 const dbConnection = require("./utils/DBconnection");
@@ -15,12 +18,11 @@ app.use(require("cors")());
 const routePrefix = "api";
 
 app.use(`/${routePrefix}`, authRoute);
-app.use(`/${routePrefix}/country`, countryRoute);
-app.use(`/${routePrefix}/state`, stateRoute);
-app.use(`/${routePrefix}/district`, districtRoute);
-
-//role route
-const roleRouter = require("./routes/role");
+app.use(`/${routePrefix}/countries`, countryRoute);
+app.use(`/${routePrefix}/states`, stateRoute);
+app.use(`/${routePrefix}/districts`, districtRoute);
+app.use(`/${routePrefix}/universities`,universityRoute);
+app.use(`/${routePrefix}/streams`,streamRoute);
 app.use(`/${routePrefix}/roles`, roleRouter);
 
 app.listen(4000, async () => {
