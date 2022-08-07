d7a8c316d89521527b66d7827dc403c0286f0662
Shubh Jangam <shubh.jangam@chitran.in>
2022-08-07 11:53:05 +0530

    Validations - Added Login Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const user = require("../controllers/user");
-
+const { registrationAuthRules, validateRegistration,loginAuthRules, validateLogin } = require('../validations/UserValidations')
 const router = require("express").Router();
 
 router.post("/register", user.register);
-router.post("/login", user.login);
+router.post("/login",loginAuthRules(), validateLogin, user.login);
 
 module.exports = router;
