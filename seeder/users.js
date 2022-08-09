b2336fa24f304fdccfeecba31fd532ef16460313
mohit-cfc <santosh.mohit9@gmail.com>
2022-08-09 12:35:16 +0530

    added user seeds for all roles

--- seeder/users.js
+++ seeder/users.js
@@ -10,27 +10,68 @@ const connectToMongo = async () => {
 connectToMongo();
 
 const userModel = require("../schema/users");
+const { log } = require("console");
+
+const seeders = [
+  {
+    role: "super-admin",
+    email: "bob@gmail.com",
+  },
+  {
+    role: "ugc-admin",
+    email: "sally@gmail.com",
+  },
+  {
+    role: "fa-admin",
+    email: "bradely@gmail.com",
+  },
+  {
+    role: "fa-project-coordinator",
+    email: "pawan@gmail.com",
+  },
+  {
+    role: "hei-admin",
+    email: "suresh@gmail.com",
+  },
+  {
+    role: "hei-project-coordinator",
+    email: "mason@gmail.com",
+  },
+  {
+    role: "project-member",
+    email: "brian@gmail.com",
+  },
+  {
+    role: "university-admin",
+    email: "rahul@gmail.com",
+  },
+  {
+    role: "hei-spoc",
+    email: "friday@gmail.com",
+  },
+];
+
+const seedUser = [];
 
 const seedData = async () => {
-  const role = await roleModel.findOne({ name: "super-admin" });
-
-  const seedUser = [
-    {
-      email: "santosh.mohit9@gmail.com",
+  for (let seeder of seeders) {
+    const role = await roleModel.findOne({ name: seeder.role });
+    seedUser.push({
+      email: seeder.email,
       hash: "",
-      firstName: "Mohit",
-      lastName: "Santosh",
+      firstName: "Genric Name",
+      lastName: "Generic Last Name",
       mobile: "5869437212",
       dob: "2018-12-10T13:45:00.000Z",
       salt: "",
       gender: "male",
       role: role._id,
-    },
-  ];
+    });
+  }
 
   const importData = async () => {
     try {
-      await userModel.deleteMany();
+      // await userModel.deleteMany();
       await userModel.insertMany(seedUser);
       let users = await userModel.find();
       for (let i in users) {
