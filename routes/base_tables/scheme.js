8de672da667962c67d2d87103b5a227952fe6e72
Shubh Jangam <shubh.jangam@chitran.in>
2022-08-19 11:53:25 +0530

    [REFACTOR] - Scheme Roles Check added

--- routes/base_tables/scheme.js
+++ routes/base_tables/scheme.js
@@ -6,7 +6,7 @@ router.get("/", scheme.index);
 router.post("/", scheme.create);
 router.get("/:id/edit", scheme.edit);
 router.put("/:id", scheme.update);
-router.delete("/:id", scheme.delete);
+// router.delete("/:id", scheme.delete);
 router.get("/:id", scheme.show);
 
 module.exports = router;
