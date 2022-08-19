e5c4255bd5a41e6d548955bd97a448c8fa73789a
mohit-cfc <santosh.mohit9@gmail.com>
2022-08-19 23:06:14 +0530

    Added role middleware for team members

--- controllers/teamMember.js
+++ controllers/teamMember.js
@@ -3,52 +3,87 @@ const returnMessage = require("./message");
 const messages = require("../lang/messages.json");
 
 module.exports = {
-  index: async(req,res) => {
+  index: async (req, res) => {
     try {
       const teamMembers = await teamMemberModel.find({});
-      returnMessage.successMessage(res,messages.successMessages.getAllCountries,teamMembers);
+      returnMessage.successMessage(
+        res,
+        messages.successMessages.getAllCountries,
+        teamMembers
+      );
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
   create: async (req, res) => {
     try {
-      const teamMember = await teamMemberModel.create({ ...req.body });
-      returnMessage.successMessage(res,messages.successMessages.addCountry,teamMember);
+      if (req.user.role.name == "super-admin") {
+        const teamMember = await teamMemberModel.create({ ...req.body });
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.addCountry,
+          teamMember
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  edit: async(req,res) => {
+  edit: async (req, res) => {
     try {
-      const teamMember = await teamMemberModel.findOne({_id: req.params['id'] })
-      returnMessage.successMessage(res,messages.successMessages.showCountry, teamMember);
-    } catch(error) {
-      returnMessage.errorMessage(res,error);
-    }
-  },
-  update: async(req,res) => {
-    try {
-      const teamMember = await teamMemberModel.findByIdAndUpdate(req.params['id'], { ...req.body });
-      returnMessage.successMessage(res,messages.successMessages.updateCountry, teamMember);
+      if (req.user.role.name == "super-admin") {
+        const teamMember = await teamMemberModel.findOne({
+          _id: req.params["id"],
+        });
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.showCountry,
+          teamMember
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  delete: async(req,res) => {
+  update: async (req, res) => {
     try {
-      const teamMember = await teamMemberModel.remove({ '_id': req.params['id'] });
-      returnMessage.successMessage(res,messages.successMessages.deleteCountry);
+      if (req.user.role.name == "super-admin") {
+        const teamMember = await teamMemberModel.findByIdAndUpdate(
+          req.params["id"],
+          { ...req.body }
+        );
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.updateCountry,
+          teamMember
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  show: async(req,res) => {
+  delete: async (req, res) => {
     try {
-      const teamMember = await teamMemberModel.findOne({_id: req.params['id'] })
-      returnMessage.successMessage(res,messages.successMessages.showCountry, teamMember);
-    } catch(error) {
-      returnMessage.errorMessage(res,error);
+      const teamMember = await teamMemberModel.remove({
+        _id: req.params["id"],
+      });
+      returnMessage.successMessage(res, messages.successMessages.deleteCountry);
+    } catch (error) {
+      returnMessage.errorMessage(res, error);
     }
-  }
+  },
+  show: async (req, res) => {
+    try {
+      const teamMember = await teamMemberModel.findOne({
+        _id: req.params["id"],
+      });
+      returnMessage.successMessage(
+        res,
+        messages.successMessages.showCountry,
+        teamMember
+      );
+    } catch (error) {
+      returnMessage.errorMessage(res, error);
+    }
+  },
 };
--- routes/teamMember.js
+++ routes/teamMember.js
@@ -3,11 +3,12 @@ const teamMember = require("../controllers/teamMember");
 const router = require("express").Router();
 
 const { validateHei, heiValidateRules } = require("../validations/base-tables");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.get("/", teamMember.index);
-router.post("/", teamMember.create);
-router.get("/:id/edit", teamMember.edit);
-router.put("/:id", teamMember.update);
+router.post("/", roleMiddleware, teamMember.create);
+router.get("/:id/edit", roleMiddleware, teamMember.edit);
+router.put("/:id", roleMiddleware, teamMember.update);
 router.delete("/:id", teamMember.delete);
 router.get("/:id", teamMember.show);
 
