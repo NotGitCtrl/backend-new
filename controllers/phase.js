d94b8ddba7277474d289c2d9528725cd700a18f5
mohit-cfc <santosh.mohit9@gmail.com>
2022-08-19 23:13:42 +0530

    Added role middleware for phases

--- controllers/phase.js
+++ controllers/phase.js
@@ -3,52 +3,80 @@ const returnMessage = require("./message");
 const messages = require("../lang/messages.json");
 
 module.exports = {
-  index: async(req,res) => {
+  index: async (req, res) => {
     try {
       const phases = await phaseModel.find({});
-      returnMessage.successMessage(res,messages.successMessages.getAllCountries,phases);
+      returnMessage.successMessage(
+        res,
+        messages.successMessages.getAllCountries,
+        phases
+      );
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
   create: async (req, res) => {
     try {
-      const phase = await phaseModel.create({ ...req.body });
-      returnMessage.successMessage(res,messages.successMessages.addCountry,phase);
+      if (req.user.role.name == "super-admin") {
+        const phase = await phaseModel.create({ ...req.body });
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.addCountry,
+          phase
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  edit: async(req,res) => {
+  edit: async (req, res) => {
     try {
-      const phase = await phaseModel.findOne({_id: req.params['id'] })
-      returnMessage.successMessage(res,messages.successMessages.showCountry, phase);
-    } catch(error) {
-      returnMessage.errorMessage(res,error);
-    }
-  },
-  update: async(req,res) => {
-    try {
-      const phase = await phaseModel.findByIdAndUpdate(req.params['id'], { ...req.body });
-      returnMessage.successMessage(res,messages.successMessages.updateCountry, phase);
+      if (req.user.role.name == "super-admin") {
+        const phase = await phaseModel.findOne({ _id: req.params["id"] });
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.showCountry,
+          phase
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  delete: async(req,res) => {
+  update: async (req, res) => {
     try {
-      const phase = await phaseModel.remove({ '_id': req.params['id'] });
-      returnMessage.successMessage(res,messages.successMessages.deleteCountry);
+      if (req.user.role.name == "super-admin") {
+        const phase = await phaseModel.findByIdAndUpdate(req.params["id"], {
+          ...req.body,
+        });
+        returnMessage.successMessage(
+          res,
+          messages.successMessages.updateCountry,
+          phase
+        );
+      }
     } catch (error) {
-      returnMessage.errorMessage(res,error);
+      returnMessage.errorMessage(res, error);
     }
   },
-  show: async(req,res) => {
+  delete: async (req, res) => {
     try {
-      const phase = await phaseModel.findOne({_id: req.params['id'] })
-      returnMessage.successMessage(res,messages.successMessages.showCountry, phase);
-    } catch(error) {
-      returnMessage.errorMessage(res,error);
+      const phase = await phaseModel.remove({ _id: req.params["id"] });
+      returnMessage.successMessage(res, messages.successMessages.deleteCountry);
+    } catch (error) {
+      returnMessage.errorMessage(res, error);
     }
-  }
+  },
+  show: async (req, res) => {
+    try {
+      const phase = await phaseModel.findOne({ _id: req.params["id"] });
+      returnMessage.successMessage(
+        res,
+        messages.successMessages.showCountry,
+        phase
+      );
+    } catch (error) {
+      returnMessage.errorMessage(res, error);
+    }
+  },
 };
--- routes/phase.js
+++ routes/phase.js
@@ -1,13 +1,14 @@
 const phase = require("../controllers/phase");
 
 const router = require("express").Router();
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 // const { validateHei, heiValidateRules } = require("../validations/base-tables");
 
 router.get("/", phase.index);
-router.post("/", phase.create);
-router.get("/:id/edit", phase.edit);
-router.put("/:id", phase.update);
+router.post("/", roleMiddleware, phase.create);
+router.get("/:id/edit", roleMiddleware, phase.edit);
+router.put("/:id", roleMiddleware, phase.update);
 router.delete("/:id", phase.delete);
 router.get("/:id", phase.show);
 
