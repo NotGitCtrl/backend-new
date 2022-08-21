0c3bb545c307b2d5176122dcf37f862946e7e7fc
Shubh Jangam <shubh.jangam@chitran.in>
2022-08-21 13:42:40 +0530

    [REFACTOR] - State Module Done

--- routes/base_tables/state.js
+++ routes/base_tables/state.js
@@ -2,29 +2,14 @@ const state = require("../../controllers/base_tables/state");
 
 const router = require("express").Router();
 
-const {
-  validateBaseTables,
-  baseTableValidateRules,
-} = require("../../validations/base-tables");
+const { validateBaseTables, baseTableValidateRules} = require("../../validations/base-tables");
 const roleMiddleware = require("../../middleware/roleMiddleware");
 
 router.get("/", state.index);
-router.post(
-  "/",
-  baseTableValidateRules(),
-  validateBaseTables,
-  roleMiddleware,
-  state.create
-);
+router.post("/", baseTableValidateRules(), validateBaseTables,roleMiddleware,state.create);
 router.get("/:id/edit", roleMiddleware, state.edit);
-router.put(
-  "/:id",
-  baseTableValidateRules(),
-  validateBaseTables,
-  roleMiddleware,
-  state.update
-);
-router.delete("/:id", state.delete);
+router.put("/:id",baseTableValidateRules(),validateBaseTables,roleMiddleware,state.update);
+// router.delete("/:id", state.delete);
 router.get("/:id", state.show);
 
 module.exports = router;
