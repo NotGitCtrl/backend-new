38a6ae174797886ddd2f00d0ce24db16bdf69951
mohit-cfc <santosh.mohit9@gmail.com>
2022-08-21 22:42:32 +0530

    Added index for roles and small fixes

--- routes/role.js
+++ routes/role.js
@@ -1,7 +1,8 @@
 const role = require("../controllers/role");
 const router = require("express").Router();
-const { checkPermission } = require("../middleware/permissionMiddleware");
+const {checkPermission} = require("../middleware/permissionMiddleware");
 
+router.get("/", role.index);
 router.post("/", checkPermission("Sample permissions"), role.create);
 
 module.exports = router;
